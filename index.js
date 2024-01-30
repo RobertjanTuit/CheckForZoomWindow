61d537fb2baecedda461fdaa365e8f18a0492cf0
RJ Tuit <robertjan.tuit@gmail.com>
2024-01-30 12:18:26 -0800

    - Adding resetting of video - Adding sleep detection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const keylights = ['keylight-left.office.localdomain', 'keylight-right.office.lo
 
 import { windowManager } from 'node-window-manager';
 import fetch from 'node-fetch';
+import robot from "robotjs";
 
 function getLightData(on, brightness = null, temperature = null)
 {
@@ -38,6 +39,7 @@ function zoomMeeting(isMeetingNew) {
         isMeeting = isMeetingNew;
         if (isMeeting) {
             console.log('Zoom Meeting window found --> Turning on lights');
+            fixVideoAndCams();
             setLights(1);
         }   
         else {
@@ -47,8 +49,19 @@ function zoomMeeting(isMeetingNew) {
     }
 };
 
+function fixVideoAndCams() {
+    console.log('Fixing video and cams...');
+    robot.keyTap('j', ['control', 'alt', 'shift']);
+    robot.keyTap('l', ['control', 'alt', 'shift']);
+    setTimeout(() => {
+        robot.keyTap('j', ['control', 'alt', 'shift']);
+        robot.keyTap('k', ['control', 'alt', 'shift']);
+    }, 2000);
+}
+
 function startWindowCheck()
 {
+    console.log('Checking for zoom windows...');
     setInterval(() => {
         var zoomWindows = windowManager.getWindows().filter((window) => {
         return window.getTitle()?.indexOf('Zoom Meeting') !== -1;
@@ -62,7 +75,32 @@ function startWindowCheck()
     }, 2000);
 }
 
+const suspendCheckInterval = 2000;
+const suspendCheckMargin = 1000;
+let ts;
+function startSuspendSleepCheck() {
+    console.log('Checking for suspend/sleep/hibernate...');
+    ts = new Date().getTime();
+    suspendSleepCheckLoop(ts);
+}
+
+function suspendSleepCheckLoop(ts) {
+    setTimeout(() => {
+        let newTs = new Date().getTime();
+        const tsDiff = newTs - ts;
+        if (tsDiff > suspendCheckInterval + suspendCheckMargin) {
+            console.log('System is waking up --> Turning on lights');
+            setTimeout(fixVideoAndCams, 6000);
+        }
+        ts = newTs;
+        suspendSleepCheckLoop();
+    }, suspendCheckInterval);
+    return ts;
+}
+
+
 startWindowCheck();
+startSuspendSleepCheck();
 //process.stdin.setRawMode( true );
 //process.stdin.setEncoding( 'utf8' );
 // on any data into stdin
